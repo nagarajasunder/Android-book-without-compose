c82abe48caa1306cc7a9f5a180540fc708ed72f3
nagarajasunder <gowthamvj15@gmail.com>
2022-10-25 13:02:00 +0530

    AWOC-3

--- app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
+++ app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
@@ -1,13 +1,52 @@
 package com.geekydroid.androidbook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.geekydroid.androidbook.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupTabLayout()
+
+    }
+
+    private fun setupTabLayout() {
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab?.text == "Attempted Bills") {
+                    Toast.makeText(
+                        applicationContext,
+                        "Attempted Bills selected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Not Attempted Bills selected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
     }
 }
