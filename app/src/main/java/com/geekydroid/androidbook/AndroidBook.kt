bf34109e89283d62a28e9c3522efedcb6bd6b8c3
nagarajasunder <gowthamvj15@gmail.com>
2023-03-13 17:40:35 +0530

    generic-branch

--- app/src/main/java/com/geekydroid/androidbook/AndroidBook.kt
+++ app/src/main/java/com/geekydroid/androidbook/AndroidBook.kt
@@ -1,8 +1,35 @@
 package com.geekydroid.androidbook
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+
+const val SAMPLE_NOTIFICATION_CHANNEL_ID = "SAMPLE_NOTIFICATION_CHANNEL_ID"
+const val SAMPLE_NOTIFICATION_CHANNEL_NAME = "SAMPLE_NOTIFICATION_CHANNEL"
+const val SAMPLE_NOTIFICATION_ID = -4
+const val SERVICE_CHANNEL_ID = "NOTIFICATION_SERVICE_CHANNEL"
+const val SERVICE_NOTIFICATION_ID = -5
 
 @HiltAndroidApp
 class AndroidBook : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                SAMPLE_NOTIFICATION_CHANNEL_ID,
+                SAMPLE_NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "Sample Notification Channel"
+            val notificationManger = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManger.createNotificationChannel(channel)
+        }
+    }
 }
--- app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
+++ app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
@@ -1,30 +1,72 @@
 package com.geekydroid.androidbook
 
+import android.app.NotificationManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.databinding.DataBindingUtil
 import com.geekydroid.androidbook.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import org.joda.time.DateTime
-import org.joda.time.Days
+import org.joda.time.LocalDateTime
 import org.joda.time.format.DateTimeFormat
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    private val attachmentDate = "2022-10-20"
-    private val initialDate = DateTime().withTime(0,0,0,0)
+    val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            Toast.makeText(this, "Notification Permission Granted $result", Toast.LENGTH_SHORT)
+                .show()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        initUi()
 
     }
 
+    private fun initUi() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+//        }
+        binding.btnPostNotification.setOnClickListener {
+            postNotification()
+        }
+        binding.btnStartService.setOnClickListener {
+            val intent = Intent(this, SampleService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+        }
+    }
+
+    private fun postNotification() {
+        val notification = NotificationCompat.Builder(this, SAMPLE_NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Sample Notification")
+            .setContentText(getContentText())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(SAMPLE_NOTIFICATION_ID, notification.build())
+    }
+
+    private fun getContentText(): String {
+        val now = LocalDateTime.now()
+        val formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss -a")
+        return "Notification Showing in ${formatter.print(now)}"
+    }
 
 }
