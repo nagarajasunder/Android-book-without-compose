24028183bf657eeb3dc853a8c5624053fd74c559
nagarajasunder <gowthamvj15@gmail.com>
2023-03-13 14:50:46 +0530

    development

--- app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
+++ app/src/main/java/com/geekydroid/androidbook/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
-import androidx.recyclerview.widget.RecyclerView
 import com.geekydroid.androidbook.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import org.joda.time.DateTime
@@ -27,29 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setContentView(binding.root)
 
-        initView()
     }
 
-    private fun isToday(initialDate: DateTime): Boolean {
-        return DateTime().withTime(0,0,0,0).millis == initialDate.millis
-    }
-
-    private fun initView() {
-        val dtf = DateTimeFormat.forPattern("yyyy-MM-dd")
-        val startDate = dtf.parseDateTime(attachmentDate)
-        Log.d("TAG", "initView: $startDate")
-        val totalCalendarDays = Days.daysBetween(startDate,DateTime().withTime(0,0,0,0)).days + 2
-        binding.horizontalDatePicker.init(totalCalendarDays+2,totalCalendarDays,initialDate)
-
-        binding.ivPreviousDay.setOnClickListener {
-            binding.horizontalDatePicker.setDate(initialDate.minusDays(1))
-        }
-
-        binding.ivNextDay.setOnClickListener {
-            if (!isToday(initialDate)) {
-                binding.horizontalDatePicker.setDate(initialDate.plusDays(1))
-            }
-        }
-    }
 
 }
